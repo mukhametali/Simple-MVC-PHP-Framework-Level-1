292e2aeebb6459bb925056d27dd28feaf62f581d
mukhametali <m.kussaiynov1996@gmail.com>
2023-04-08 13:59:02 +0600

    class Db and SQL injection protection

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -3,11 +3,19 @@
 namespace application\controllers;
 
 use application\core\Controller;
+use application\lib\Db;
 
 class MainController extends Controller
 {
     public function indexAction()
     {
+        $db = new Db;
+
+        $params = ['id' => 2];
+
+        $data = $db->row('SELECT name FROM users WHERE id =:id',$params);
+        debug($data);
+
 
         $this->view->render('Main page');
     }
